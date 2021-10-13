eb2d37fb2725bce51ca7a0019699149b68e32152
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-10-13 07:47:26 -0400

    feat: Add the demonstrate-reduce-function with TODO markers.

---
+++ source/demonstrate-reduce-function.py
@@ -0,0 +1,32 @@
+"""Demonstrate the use of the reduce higher-order function."""
+
+# TODO: Refer to the course slides for more details about these functions
+
+def plus(number_one, number_two):
+    """Add the two numbers together and return them."""
+    # TODO: add the two numbers together and return them
+
+
+def multiply(number_one, number_two):
+    """Multiply the two numbers together and return them."""
+    # TODO: multiply the two numbers together and return them
+
+
+def reduce(f, sequence, initial):
+    """Starting at the value of initial, apply f to the sequence of values."""
+    # TODO: start the result of the computation at the initial value
+    result = initial
+    # TODO: apply the function f to the values in the sequence, continuing
+    # to use the values that are return from the function
+    # TODO: note that this should work for any type of function f
+    # TODO: return the result of this computation
+
+
+# create a list of numbers to be used in the reduce computations
+numbers = [1, 2, 3, 4, 5]
+
+# TODO: add all of the numbers together using a reduce function call
+# TODO: display the result of the computation, indented by three spaces
+
+# TODO: add all of the numbers together using a reduce function call
+# TODO: display the result of the computation, indented by three spaces
