f59c4cd8efb9e313cb7433721305dd75810c7b12
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-10-13 07:43:29 -0400

    feat: Add the template version of the demonstrate-map-function.

---
+++ source/demonstrate-map-function.py
@@ -0,0 +1,26 @@
+"""Demonstrate the use of the map higher-order function."""
+
+def square(value):
+    """Compute the square of a provided number."""
+    # TODO: compute the square of the provided number and return it
+
+
+def map(f, sequence):
+    """Apply the function f to the sequence of values."""
+    # TODO: apply the function f to the sequence of values and return
+    # the result of the overall computation
+    # TODO: note that this should work for any type of function f
+
+
+# create a list of values and then square them using map
+values = (2, 3, 5, 7, 11)
+squared = map(square, values)
+# TODO: display the output of the squaring computation
+# TODO: display the result of the computation, indented by three spaces
+
+# create a list of values using the range function
+# then square the output of range with the map function
+squared_range = map(square, range(10))
+# TODO: display the output of the squaring computation
+# TODO: display the result of the computation, indented by three spaces
+print("   " + str(squared_range))
